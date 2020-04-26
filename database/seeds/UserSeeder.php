f9ff854f476b46d0e0c13029d43624d01c28fff7
Edipo Elwes <edipoelwes2@gmail.com>
2020-04-26 20:51:20 -0300

    login e dashboard

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name'           => 'Edipo Elwes',
+                'document'       => '03570590348',
+                'email'          => 'edipoelwes2@gmail.com',
+                'password'       => bcrypt('12345678'),
+                'remember_token' => Str::random(10),
+                'created_at'     => now(),
+                'updated_at'     => now(),
+            ],
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::namespace('Admin')->prefix('admin')->as('admin.')->group(function (){
+
+    /** Formulario de Login */
+   Route::get('/', 'AuthController@loginForm')->name('login');
+   Route::post('login', 'AuthController@login')->name('login.do');
+
+   /** Rotas Protegidas */
+   Route::middleware('auth')->group(function (){
+       /** Dashboard Home */
+       Route::get('home', 'AuthController@home')->name('home');
+       
+    //    Route::get('users/team', 'UserController@team')->name('users.team');
+    //    Route::resources([
+    //        'users' => 'UserController',
+    //        /** Empresa */
+    //        'companies' => 'CompanyController',
+    //        /** Categorias */
+    //        'categories' => 'CategoryController',
+    //        /** Produtos / Fraldas */
+    //        'diapers' => 'DiaperController',
+    //        /** Produtos / Lenços */
+    //        'wipes' => 'WipeController',
+    //        /** Estoque */
+    //        'stocks' => 'StockController',
+    //         /** Vendas */
+    //         'sales' => 'SaleController',
+    //    ]);
+   });
+
+   /** Logout */
+   Route::get('logout', 'AuthController@logout')->name('logout');
+});
